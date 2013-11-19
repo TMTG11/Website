4beccf172c79fb94c71828dbd0b821c97200b4eb
Maarten Paauw <maartenpaauw@gmail.com>
2013-11-19 09:54:51 +0100

    Aanpassingen button functies

--- scripts/inlogcheck.php
+++ scripts/inlogcheck.php
@@ -1,9 +1,26 @@
+# Changelog :
+# Maarten Paauw : Aanmaken van functies voor de buttons inloggen en registreren.
+
 <?php
-	function inlogbutton {
+	require_once('../connection.php');
+	
+	function inloggen_button {
 		if ($ingelogd) {
-			echo "Account";
+			$link = "http://www.google.nl/?true=true";
+			$tekst = "Account";
 		} else {
-			echo "Inloggen";
+			$link = "http://www.google.nl/?false=false";
+			$tekst = "Inloggen";
+		}
+	}
+	
+	function registreren_button {
+		if($ingelogd) {
+			$link = "http://www.google.nl/?true=true";
+			$tekst = "Posten";
+		} else {
+			$link = "http://www.google.nl/?false=false";
+			$tekst = "Registreren";
 		}
 	}
 ?>
