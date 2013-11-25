da81f436b53666e50f552a4886a9841a39018697
Maarten Paauw <maartenpaauw@gmail.com>
2013-11-25 13:31:33 +0100

    Aanpassingen laatste kaartje

--- scripts/kaartjes.php
+++ scripts/kaartjes.php
@@ -2,10 +2,18 @@
 	/*
 		25-11-2013 - Mies en Maarten : Begin functie.
 	*/
+	
+	require_once('../connection.php');
+	$tabelkaartjes = "kaartjes";
+	
 	function laatste_kaartje() {
-		$array["naam"] = 'Maarten Paauw';
-		$array["tekst"] = 'Dit is de tekst';
-		$array["datum"] = '02-01-1993';
+		$opdrachtlaatsgeboren = "SELECT * FROM $tabelkaartjes WHERE Bevestigd = '1' ORDER BY Geboortedatum DESC LIMIT 0, 1";
+		$result = mysql_query($opdrachtlaatsgeboren);
+		$Rij = mysql_fetch_array($result);
+		
+		$array["naam"] = $Rij['Naam'];
+		$array["tekst"] = $Rij['VrijeTekst'];
+		$array["datum"] = $Rij['Geboortedatum'];
 		
 		return($array);
 	}
