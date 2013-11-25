96626dc208f63af1ce23cb22eb235fe703d5be13
Maarten Paauw <maartenpaauw@gmail.com>
2013-11-25 13:25:11 +0100

    Laatste kaartje
    Het uitlezen van het laatste kaartje.

---
+++ scripts/kaartjes.php
@@ -0,0 +1,12 @@
+<?php
+	/*
+		25-11-2013 - Mies en Maarten : Begin functie.
+	*/
+	function laatste_kaartje() {
+		$array["naam"] = 'Maarten Paauw';
+		$array["tekst"] = 'Dit is de tekst';
+		$array["datum"] = '02-01-1993';
+		
+		return($array);
+	}
+?>
