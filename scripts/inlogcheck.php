5deec3367d361ce30ae6c20d861616d054c3748d
Maarten Paauw <maartenpaauw@gmail.com>
2013-11-25 13:02:21 +0100

    Functie uigewerkt
    Toegevoegd check voor logo met geslacht en de buttons

--- scripts/inlogcheck.php
+++ scripts/inlogcheck.php
@@ -1,36 +1,50 @@
-# Changelog :
-# Maarten Paauw : Aanmaken van functies voor de buttons inloggen en registreren.
-
 <?php
-	require_once('../connection.php');
+	#Changelog :
+	#Maarten Paauw : Aanmaken van functies voor de buttons inloggen en registreren.
+	#Maarten Paauw : Functie logo.
 	
-	function inloggen_button {
-		if ($ingelogd) {
-			$link = "http://www.google.nl/?true=true";
-			$tekst = "Account";
-		} else {
-			$link = "http://www.google.nl/?false=false";
-			$tekst = "Inloggen";
+	function inloggen_button($type) {
+		if($_SESSION['ingelogd']) {
+			if($type == "link"){
+				return("http://tmtg11.ict-lab.nl/website/account.php");
+			} elseif($type == "tekst") {
+				return("Account");
+			}
+		} else {			
+			if($type == "link") {
+				return("http://tmtg11.ict-lab.nl/website/login.php");
+			} elseif($type == "tekst") {
+				return("Inloggen");
+			}
 		}
 	}
 	
-	function registreren_button {
-		if($ingelogd) {
-			$link = "http://www.google.nl/?true=true";
-			$tekst = "Posten";
-		} else {
-			$link = "http://www.google.nl/?false=false";
-			$tekst = "Registreren";
+	function registreren_button($type) {
+		if($_SESSION['ingelogd']) {
+			if($type == "link"){
+				return("http://tmtg11.ict-lab.nl/website/posten.php");
+			} elseif($type == "tekst") {
+				return("Kaartje Maken");
+			}
+		} else {			
+			if($type == "link") {
+				return("http://tmtg11.ict-lab.nl/website/login.php");
+			} elseif($type == "tekst") {
+				return("Registreren");
+			}
 		}
 	}
 	
-	function logo {
-		if($jongen) {
-			$link = "http://www.google.nl/?true=true"; 
-		} else if($meisje) {
-			$link = "http://www.google.nl/?true=true";
+	function logo($type) {
+		$array = $_SESSION["userinfo"];
+		$geslacht = $array["Geslacht"];
+		
+		if($geslacht == "M") {
+			return("http://tmtg11.ict-lab.nl/website/img/logo/logo_blauw.png");
+		} else if($geslacht == "V") {
+			return("http://tmtg11.ict-lab.nl/website/img/logo/logo_roze.png");
 		} else {
-			$link = "http://www.google.nl/?true=true";
+			return("http://tmtg11.ict-lab.nl/website/img/logo/logo.png");
 		}
 	}
 ?>
