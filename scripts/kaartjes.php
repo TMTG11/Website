429cc6c1fc68116910ff321d56536d11f736506a
Maarten Paauw <maartenpaauw@gmail.com>
2013-11-26 09:03:53 +0100

    Uitbreiding aan script

--- scripts/kaartjes.php
+++ scripts/kaartjes.php
@@ -16,8 +16,45 @@
 		$Rij = mysql_fetch_array($result);
 				
 		$array["naam"] = $Rij['Voornaam'];
+		$array["tussenvoegsel"] = $Rij['Tussenvoegsel'];
+		$array["achternaam"] = $Rij['Achternaam'];
 		$array["tekst"] = $Rij['VrijeTekst'];
 		$array["datum"] = $Rij['Geboortedatum'];
+		$array["geslacht"] = $Rij['Geslacht'];
+		
+		return($array);
+	}
+	
+	function laatste_jongen() {
+		$tabelkaartjes = "Kaartjes";
+		
+		$opdrachtlaatstgeboren = "SELECT * FROM $tabelkaartjes WHERE Bevestigd = '1' AND Geslacht = 'M' ORDER BY Geboortedatum DESC LIMIT 1";
+		$result = mysql_query($opdrachtlaatstgeboren);
+		$Rij = mysql_fetch_array($result);
+				
+		$array["naam"] = $Rij['Voornaam'];
+		$array["tussenvoegsel"] = $Rij['Tussenvoegsel'];
+		$array["achternaam"] = $Rij['Achternaam'];
+		$array["tekst"] = $Rij['VrijeTekst'];
+		$array["datum"] = $Rij['Geboortedatum'];
+		$array["geslacht"] = $Rij['Geslacht'];
+		
+		return($array);
+	}
+	
+	function laatste_meisje() {
+		$tabelkaartjes = "Kaartjes";
+		
+		$opdrachtlaatstgeboren = "SELECT * FROM $tabelkaartjes WHERE Bevestigd = '1' AND Geslacht = 'V' ORDER BY Geboortedatum DESC LIMIT 1";
+		$result = mysql_query($opdrachtlaatstgeboren);
+		$Rij = mysql_fetch_array($result);
+				
+		$array["naam"] = $Rij['Voornaam'];
+		$array["tussenvoegsel"] = $Rij['Tussenvoegsel'];
+		$array["achternaam"] = $Rij['Achternaam'];
+		$array["tekst"] = $Rij['VrijeTekst'];
+		$array["datum"] = $Rij['Geboortedatum'];
+		$array["geslacht"] = $Rij['Geslacht'];
 		
 		return($array);
 	}
